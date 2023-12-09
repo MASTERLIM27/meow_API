d883ef0f483158f438696c41dc27712975a5142f
MASTERLIM27 <andreaslim2002@gmail.com>
2023-12-09 22:10:26 +0700

    Restful API Product
    Product CRUD Done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,98 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Validator;
 
 class ProductController extends Controller
 {
-    //
+
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
+
+    public function store(Request $request){
+        $product = new Product;
+        $product-> name = $request->name;
+        $product-> image_path = $request->image_path;
+        $product-> price = $request->price;
+        $product-> stock = $request->stock;
+        $product-> type = $request->type;
+        $product->save();
+
+        return response()->json([
+            "message" => "Product Added."
+        ], 201);
+    }
+
+    public function show($id){
+        $product = Product::find($id);
+        if (!empty($product)) {
+            return response()->json($product);
+        }
+        else {
+            return response()->json([
+                "message" => "Product not found."
+            ], 404);
+        }
+    }
+
+    public function update(Request $request, $id){
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'image_path' => 'required',
+            'price' => 'required',
+            'stock' => 'required',
+            'type' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'message' => $validator->messages(),
+            ], 422);
+        } else {
+            $product = Product::find($id);
+
+            if ($product) {
+                $product->update([
+                    'name' => $request->name,
+                    'image_path' => $request->image_path,
+                    'price' => $request->price,
+                    'stock' => $request->stock,
+                    'type'=> $request->type
+                ]);
+
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Product updated.'
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 404,
+                    'message' => "Product with ID $id not found."
+                ], 404);
+            }
+        }
+    }
+
+    public function destroy($id){
+        if (Product::where('id', $id)->exists()) {
+            $product = Product::find($id);
+            $product->delete();
+
+            return response()->json([
+                "message" => "Product deleted."
+            ], 202);
+        }
+        else {
+            return response()->json([
+                "message" => "Product not found."
+            ], 404);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/product', [ProductController::class, 'index']);
+Route::get('/product/{id}', [ProductController::class, 'show']);
+Route::post('/product', [ProductController::class, 'store']);
+Route::put('/product/{id}', [ProductController::class, 'update']);
+Route::delete('/product/{id}', [ProductController::class, 'destroy']);
